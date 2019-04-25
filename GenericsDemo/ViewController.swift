cdc8069249e074ad2263ab8f0bb46944919203dc
Manjunath C <manjuhere@me.com>
2019-04-26 00:51:56 +0530

    Initial Commit

---
+++ GenericsDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GenericsDemo
+//
+//  Created by Manjunath Chandrashekar on 26/04/19.
+//  Copyright © 2019 manjunath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
